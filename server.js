f7487da936d7d4f1d7c8ea1c2e2b7b74dc9f0d2f
cesar ortiz <ceortiz98@hotmail.com>
2018-08-01 21:43:49 -0400

    fixing issue with deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('/dist/<name-of-app>'));
 
 
 
