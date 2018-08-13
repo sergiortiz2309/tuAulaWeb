b830e60edd530d5c6934c75d45dfc30344896e72
cesar ortiz <ceortiz98@hotmail.com>
2018-08-12 21:34:38 -0400

    Added usuariosService

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../../services/usuario.service';
+import { Usuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-usuario',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioComponent implements OnInit {
 
-  constructor() { }
+  public usuario = new Usuario();
+
+  constructor(private usuariosService: UsuariosService) { }
 
   ngOnInit() {
+    this.usuariosService.getUsuario().subscribe(data => this.usuario = data);
   }
-
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsuarioService {
+export class UsuariosService {
+  apiService: any;
 
   constructor() { }
+  getUsuario(): Observable<Usuario> {
+
+    return new Observable( (observer) => {
+        observer.next(this.getFakeUsuario())
+    });
+  }
+
+  private getFakeUsuario(){
+    
+    let usuario = new Usuario();
+    usuario.apellido = "aaa";
+    usuario.direccion = "sss";
+    usuario.categoriasPreferidas = [{id : 1, nombre : "Idiomas"}];
+    usuario.email = "dkdkd";
+    usuario.nombre = "cesar";
+    return usuario;
+  }
+
 }
