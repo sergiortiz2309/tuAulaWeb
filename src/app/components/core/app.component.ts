b9fd744aeb353514afb7a4eaa195d83d1919fb3d
cesar ortiz <ceortiz98@hotmail.com>
2018-08-15 22:05:10 -0400

    worked on sidenav ... in progress

--- src/app/components/core/app.component.ts
+++ src/app/components/core/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  sidenavOpened : boolean;
+  isLandingPage : boolean;
 
-  sidenavOpened : boolean = true;
+  constructor(public apiService: ApiService, 
+              private router : Router ) {
+  }
 
-  constructor(public apiService: ApiService) {
+  ngOnInit(): void {
+      this.router
+            .events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe( () => {
+              this.isLandingPage = false;
+              if (this.router.url == "/"){
+                this.isLandingPage = true;
+              } 
+              if (this.router.url == "/" || this.router.url == "/login" || this.router.url == "/registro"){
+                this.sidenavOpened = false;
+              } 
+            })
   }
 }
